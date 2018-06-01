612050a5c5f8dbfc388b3d553ecab716223e28f4
Shmuel Honig <shonig@gmail.com>
2018-06-01 10:15:13 -0400

    Update roster route so that team name is in url instead of team id

--- baseball_catalog.py
+++ baseball_catalog.py
@@ -22,11 +22,13 @@ def showTeams():
     teams = session.query(Teams).order_by(asc(Teams.name))
     return render_template('teams.html', teams=teams)
 
-@app.route('/<team_id>/')
-@app.route('/<team_id>/roster/')
-def showRoster(team_id):
+@app.route('/<team_name>/')
+@app.route('/<team_name>/roster/')
+def showRoster(team_name):
+    team = session.query(Teams).filter_by(name=team_name).all()
+    team_id = team[0].id
     roster = session.query(Players).filter_by(team_id=team_id).order_by(asc(Players.name))
-    return render_template('roster.html', roster=roster, team_id=team_id)
+    return render_template('roster.html', roster=roster, team=team)
 
 
 if __name__ == '__main__':
