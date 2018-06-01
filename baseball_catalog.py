318ce8b4b335a86963d0854a5ed2879684952afc
Shmuel Honig <shonig@gmail.com>
2018-06-01 12:24:08 -0400

    Break up line that was too long

--- baseball_catalog.py
+++ baseball_catalog.py
@@ -28,7 +28,8 @@ def showTeams():
 def showRoster(team_name):
     team = session.query(Teams).filter_by(name=team_name).all()
     team_id = team[0].id
-    roster = session.query(Players).filter_by(team_id=team_id).order_by(asc(Players.name))
+    roster = session.query(Players).filter_by(team_id=team_id).\
+             order_by(asc(Players.name))
     return render_template('roster.html', roster=roster, team=team)
 
 
